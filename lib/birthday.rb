9f2e5eae70eb748b3820ec78eb8ffd8b98a4b1bf
Joshua Williams <jmw0426@hotmail.com>
2020-03-29 02:28:53 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,13 +4,13 @@
 #   "Amanda" => 27
 # }
 
-# def happy_birthday(birthday_kids)
-#   def happy_birthday(birthday_kids)
-#   birthday_kids.each do |kids_name, age|
-#     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-#   end
-# end
-# end
+def happy_birthday(birthday_kids)
+  def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
+end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
